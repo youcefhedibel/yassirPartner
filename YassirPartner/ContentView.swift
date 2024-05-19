dd2d0d1b799aa1c554c9e35005714f79fdc27d82
youcef <youyou_dz1@live.fr>
2024-05-19 09:19:01 +0100

    init__--__git

---
+++ YassirPartner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YassirPartner
+//
+//  Created by macbook  on 19/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YassirPartner/YassirPartnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YassirPartnerApp.swift
+//  YassirPartner
+//
+//  Created by macbook  on 19/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct YassirPartnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
